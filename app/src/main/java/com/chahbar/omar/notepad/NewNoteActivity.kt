3769659e0d1dde7084f981780afa2aa4889bbbf5
Omar-Chahbar <ochahba@uwo.ca>
2018-06-26 22:42:15 -0400

    added empty activity for new notes

--- app/src/main/java/com/chahbar/omar/notepad/NewNoteActivity.kt
+++ app/src/main/java/com/chahbar/omar/notepad/NewNoteActivity.kt
@@ -3,12 +3,11 @@ package com.chahbar.omar.notepad
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-
-class MainActivity : AppCompatActivity() {
+class NewNoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_newnote)
     }
 
 
