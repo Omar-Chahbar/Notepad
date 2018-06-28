92741ec92dc210b0d89056a347ceaa397538046a
Omar-Chahbar <ochahba@uwo.ca>
2018-06-27 22:51:38 -0400

    Added stub methods

--- app/src/main/java/com/chahbar/omar/notepad/MainActivity.kt
+++ app/src/main/java/com/chahbar/omar/notepad/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.chahbar.omar.notepad
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +13,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadNotes()
+
+        addNote.setOnClickListener {_: View ->
+             Toast.makeText(this@MainActivity, "This is a Toast Message", Toast.LENGTH_SHORT).show()
+             val intent = Intent(this@MainActivity,NewNote::class.java)
+             startActivity(intent)
+        }
+    }
+
+    private fun loadNotes() {
+//        "Read data from SQLite and populate rows in table" +
+//                "check if display preview is checked and set visibility accordingly" +
+//                "SQL fields: title, favourite, preview, text"
     }
 
 
--- app/src/main/java/com/chahbar/omar/notepad/NewNote.kt
+++ app/src/main/java/com/chahbar/omar/notepad/NewNote.kt
@@ -3,12 +3,10 @@ package com.chahbar.omar.notepad
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class NewNoteActivity : AppCompatActivity() {
+class NewNote : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_newnote)
+        setContentView(R.layout.activity_new_note)
     }
-
-
 }
